072647347c737c06ac427d817adee17de7838032
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2020-11-20 21:45:32 +0600

    Dependency Error sloved

--- ProjectManagement.BusinessLogic/DependencyInjectionExtensions.cs
+++ ProjectManagement.BusinessLogic/DependencyInjectionExtensions.cs
@@ -8,7 +8,10 @@ namespace ProjectManagement.BusinessLogic
         public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
         {
             services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IDonorCore, DonorCore>();
+            services.AddTransient<IProjectBeneficiaryTypeCore, ProjectBeneficiaryTypeCore>();
             services.AddTransient<IProjectSectorCore, ProjectSectorCore>();
+            services.AddTransient<IProjectStatusCore, ProjectStatusCore>();
             return services;
         }
     }
