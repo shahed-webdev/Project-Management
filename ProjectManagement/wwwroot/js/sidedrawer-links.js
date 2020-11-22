a3935711b411b56ebf260d8c38db2450b864d70b
muhebollah <muhebollah.diu@gamil.com>
2020-11-22 12:52:21 +0600

    remove sector

--- ProjectManagement/wwwroot/js/sidedrawer-links.js
+++ ProjectManagement/wwwroot/js/sidedrawer-links.js
@@ -26,13 +26,6 @@ let linkData = [{
             "Title": "Project Beneficiary",
             "IconClass": null
         }, {
-            "LinkID": 4,
-            "SN": 4,
-            "Controller": "Settings",
-            "Action": "ProjectSector",
-            "Title": "Project Sector",
-            "IconClass": null
-        }, {
             "LinkID": 5,
             "SN": 5,
             "Controller": "Settings",
@@ -71,92 +64,7 @@ let linkData = [{
     }];
 
 //selectors
-const menuItem = document.getElementById("menuItem")
-
-
-//get data from server
-const getMenuData = function () {
-    appendMenuDOM()
-    setNavigation()
-    //const url = ""
-    //axios.get(url)
-    //    .then(response => {
-    //        appendMenuDOM(response.data)
-    //        setNavigation()
-    //    })
-    //    .catch(err => console.log(err))
-}
-
-//create links
-const createLinks = function (links) {
-    const fragment = document.createDocumentFragment();
-    links.forEach(link => {
-        const anchor = document.createElement('a');
-        anchor.classList.add('links')
-        anchor.href = `/${link.Controller}/${link.Action}`
-        anchor.textContent = link.Title
-
-        const li = document.createElement('li');
-        li.appendChild(anchor)
-
-        fragment.appendChild(li)
-    });
-
-    return fragment
-}
-
-//create link li
-const linkCategory = function (category, iconCss, links) {
-    const span = document.createElement('span');
-    span.className = iconCss
-
-    const ico = document.createElement('i');
-    ico.classList.add('fas', 'fa-caret-right')
-
-    const strong = document.createElement('strong');
-    strong.appendChild(span)
-    strong.appendChild(ico)
-    strong.appendChild(document.createTextNode(category))
-
-    const ul = document.createElement('ul');
-    ul.classList.add('sub-menu')
-    ul.appendChild(links)
-
-    const li = document.createElement('li');
-    li.appendChild(strong)
-    li.appendChild(ul)
-
-    return li
-}
-
-//append link to DOM
-const appendMenuDOM = function (/*linkData*/) {
-    const fragment = document.createDocumentFragment();
-
-    const span = document.createElement('span');
-    span.className = 'fas fa-tachometer-alt'
-
-    const anchor = document.createElement('a');
-    anchor.classList.add('links')
-    anchor.href = '/Dashboard/Index'
-    anchor.textContent = 'Dashboard'
-
-    const strong = document.createElement('strong');
-    strong.appendChild(span)
-    strong.appendChild(anchor)
-
-    const li = document.createElement('li');
-    li.appendChild(strong)
-
-    fragment.appendChild(li)
-
-    linkData.forEach(item => {
-        const link = linkCategory(item.Category, item.IconClass, createLinks(item.links))
-        fragment.appendChild(link)
-    });
-
-    menuItem.appendChild(fragment)
-}
+const menuItem = document.getElementById("menuItem");
 
 //active current url
 function setNavigation() {
@@ -165,7 +73,7 @@ function setNavigation() {
 
     path = path.replace(/\/$/, "")
     path = decodeURIComponent(path)
-
+   
     links.forEach(link => {
         const href = link.getAttribute('href')
 
@@ -195,5 +103,4 @@ const linkCategoryClicked = function (evt) {
 //event listener
 menuItem.addEventListener("click", linkCategoryClicked)
 
-//on load
-getMenuData()
+setNavigation();
