50dbba60e071bf723959fcd4232c32d755094755
muhebollah <muhebollah.diu@gamil.com>
2020-12-03 23:27:01 +0600

    add indirector 1st step

--- ProjectManagement/Controllers/ProjectLogFrameController.cs
+++ ProjectManagement/Controllers/ProjectLogFrameController.cs
@@ -13,17 +13,19 @@ namespace ProjectManagement.Controllers
     public class ProjectLogFrameController : Controller
     { 
         private readonly ILogFrameCore _logFrame;
+        private readonly ILogFrame1stStepCore _logFrameStep1;
         private readonly IProjectCore _project;
         private readonly IProjectSectorCore _sector;
 
-        public ProjectLogFrameController(ILogFrameCore logFrame, IProjectCore project, IProjectSectorCore sector)
+        public ProjectLogFrameController(ILogFrameCore logFrame, IProjectCore project, IProjectSectorCore sector, ILogFrame1stStepCore logFrameStep1)
         {
             this._logFrame = logFrame;
             _project = project;
             _sector = sector;
+            _logFrameStep1 = logFrameStep1;
         }
 
-        //Log Frame
+        //****Log Frame****
         public IActionResult LogFrame(int? id)
         {
             if (!id.HasValue) return RedirectToAction($"Features", $"Projects");
@@ -52,7 +54,9 @@ namespace ProjectManagement.Controllers
             return Json(response);
         }
 
-        //Log Frame Indicator
+
+
+        //****Log Frame Indicator****
         public IActionResult LogFrameIndicator(int? id)
         {
             if (!id.HasValue) return RedirectToAction($"Features", $"Projects");
@@ -64,6 +68,21 @@ namespace ProjectManagement.Controllers
             ViewBag.ProjectName = new SelectList(_project.Ddl(id.Value).Data, "value", "label");
 
             return View();
+        }
+
+        //POST: log frame(ajax)
+        [HttpPost]
+        public IActionResult PostLogFrameIndicator(LogFrame1stStepModel model)
+        {
+            var response = _logFrameStep1.AddorUpdate(model);
+            return Json(response);
+        }
+
+        //on project select(ajax)
+        public IActionResult GetLogFrameIndicator(int id)
+        {
+            var response = _logFrameStep1.Get(id);
+            return Json(response);
         }
     }
 }
